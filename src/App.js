0ddb7922b18eb28ecb78fe9f1358af7c8efcea38
Norwin Schäfer <norwin270@gmail.com>
2021-09-20 14:33:03 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -5,32 +5,34 @@ function App() {
   return (
     <div className="App">
       <div class="container-fluid">
-      <form class="form-inline">
+        <form class="form-inline">
           <label class="sr-only" for="inlineFormInputName2">Name</label>
-          <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe"/>
+          <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe" />
 
-          <label class ="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-          <div class ="input-group mb-2 mr-sm-2">
-          <div class ="input-group-prepend">
-          <div class ="input-group-text">@</div>
-          </div>
-          <input type ="text" class ="form-control" id="inlineFormInputGroupUsername2" placeholder="Username"/>
+          <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+          <div class="input-group mb-2 mr-sm-2">
+            <div class="input-group-prepend">
+              <div class="input-group-text">@</div>
+            </div>
+            <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username" />
           </div>
         </form>
 
         <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-          <label class="form-check-label" for="inlineRadio1">1</label>
+          <label class="form-check-label" for="inlineRadio1">React Kurs</label>
         </div>
         <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-          <label class="form-check-label" for="inlineRadio2">2</label>
+          <label class="form-check-label" for="inlineRadio2">JavaScript Kurs</label>
         </div>
         <div class="form-check form-check-inline">
           <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" disabled />
-          <label class="form-check-label" for="inlineRadio3">3 (disabled)</label>
+          <label class="form-check-label" for="inlineRadio3">Angular Kurs</label>
         </div>
-        <button type ="submit" class ="btn btn-primary mb-2">Submit</button>
+        <div class="p-2">
+          <button type="submit" class="btn btn-primary mb-2">Submit</button>
+        </div>
       </div>
     </div>
   );
